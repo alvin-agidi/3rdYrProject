b86d12d137b715e982cd0ce5c9e3e110f9ede41a
alvin-agidi <alvin.agidi@gmail.com>
2024-02-11 01:50:31 +0000

    Basic profile with content loaded from database

--- src/screens/main/ProfileScreen.tsx
+++ src/screens/main/ProfileScreen.tsx
@@ -1,17 +1,55 @@
 import React from "react";
-import { View, Text, Image, FlatList } from "react-native";
+import { StyleSheet, View, Text, Image, FlatList } from "react-native";
 import { Video } from "expo-av";
 import { connect } from "react-redux";
 
 function ProfileScreen(props) {
 	const { currentUser, posts } = props;
-	console.log({ currentUser, posts });
+	console.log({ posts });
 	return (
-		<View>
-			<Text>Profile</Text>
+		<View style={styles.container}>
+			<View style={styles.infoContainer}>
+				<Text>{currentUser.username}</Text>
+				<Text>{currentUser.email}</Text>
+			</View>
+			<View style={styles.galleryContainer}>
+				<FlatList
+					horizontal={false}
+					numColumns={3}
+					data={posts}
+					renderItem={({ item }) => (
+						<View style={styles.imageContainer}>
+							<Image
+								source={{ uri: item.downloadURL }}
+								style={styles.image}
+							/>
+						</View>
+					)}
+				/>
+			</View>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		margin: 0,
+	},
+	infoContainer: {
+		margin: 0,
+	},
+	galleryContainer: {
+		margin: 0,
+	},
+	imageContainer: {
+		flex: 1 / 3,
+	},
+	image: {
+		flex: 1,
+		aspectRatio: 1 / 1,
+	},
+});
 
 const mapStateToProps = (store) => ({
 	currentUser: store.userState.currentUser,
