cf96caced588e2fb6624796bb2ddca07969d859e
alvin-agidi <alvin.agidi@gmail.com>
2024-02-19 02:07:08 +0000

    Add access to flask endpoint

--- src/screens/main/PublishPostScreen.tsx
+++ src/screens/main/PublishPostScreen.tsx
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
-import { View, TextInput, Image, Button } from "react-native";
+import { StyleSheet, View, TextInput, Image } from "react-native";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/storage";
 import "firebase/compat/firestore";
 import { useNavigation } from "@react-navigation/core";
+import { PressableButton } from "../../components/PressableButton";
+import globalStyles from "../../styles";
+import { ResizeMode, Video } from "expo-av";
+
+async function detectExercises(videoURL: string) {
+	return await fetch("http://143.47.229.158:5000/detectExercises", {
+		method: "POST",
+		mode: "cors",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			videoURL,
+		}),
+	}).catch((error: any) => {
+		console.error("Error:", error);
+	});
+}
 
 export default function PublishPost(props: any) {
 	const navigation = useNavigation();
@@ -53,8 +71,22 @@ export default function PublishPost(props: any) {
 				caption,
 				createdAt: firebase.firestore.FieldValue.serverTimestamp(),
 			})
-			.then(() => {
-				navigation.popToTop();
+			.then((doc) => {
+				if (isVideo) {
+					detectExercises(mediaURL).then((response) => {
+						firebase
+							.firestore()
+							.collection("users")
+							.doc(firebase.auth().currentUser!.uid)
+							.collection("posts")
+							.doc(doc.id)
+							.set(
+								{ exercises: JSON.stringify(response) },
+								{ merge: true }
+							);
+					});
+				}
+				// navigation.popToTop();
 				navigation.navigate("Profile", {
 					uid: firebase.auth().currentUser!.uid,
 				});
@@ -62,15 +94,40 @@ export default function PublishPost(props: any) {
 	}
 
 	return (
-		<View>
-			<Image source={{ uri: mediaUri }} />
+		<View style={styles.container}>
 			<TextInput
 				placeholder="Write a caption..."
 				onChangeText={(caption) => {
 					setCaption(caption);
 				}}
+				style={globalStyles.textInput}
 			/>
-			<Button title="Publish" onPress={uploadMedia} />
+			<PressableButton onPress={uploadMedia} text="Publish" />
+
+			{isVideo ? (
+				<Video
+					style={styles.media}
+					source={{ uri: mediaUri }}
+					useNativeControls
+					resizeMode={ResizeMode.CONTAIN}
+					isLooping
+				/>
+			) : (
+				<Image source={{ uri: mediaUri }} style={styles.media} />
+			)}
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		gap: 10,
+		padding: 10,
+	},
+	media: {
+		flex: 1,
+		alignSelf: "stretch",
+		borderRadius: 5,
+	},
+});
