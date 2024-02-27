d3951c455ab6682701167f5941ae057b0a2a7cf8
alvin-agidi <alvin.agidi@gmail.com>
2024-02-27 18:06:07 +0000

    Added new label component

---
+++ src/components/Label.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import globalStyles from "../globalStyles";
+
+export function Label(props: any): JSX.Element {
+	return (
+		<View {...props} style={styles.label}>
+			<Text style={globalStyles.bold}>{props.text}</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	label: {
+		fontSize: 20,
+		borderColor: "deepskyblue",
+		borderWidth: 2,
+		borderRadius: 5,
+		padding: 5,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	labelBox: {
+		flexDirection: "row",
+		gap: 5,
+	},
+});
