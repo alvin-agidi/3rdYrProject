afa052b57f1264946003e57066d3c7faf6113227
alvin-agidi <alvin.agidi@gmail.com>
2024-02-27 03:23:22 +0000

    Feed requests expo notifications token

--- src/screens/main/FeedScreen.tsx
+++ src/screens/main/FeedScreen.tsx
@@ -1,27 +1,62 @@
-import React, { Component, useEffect, useState } from "react";
-import {
-	StyleSheet,
-	View,
-	Text,
-	Image,
-	FlatList,
-	TouchableOpacity,
-} from "react-native";
-import { Video, ResizeMode } from "expo-av";
+import React, { Component } from "react";
+import { Platform } from "react-native";
 import { connect } from "react-redux";
-import { useNavigation } from "@react-navigation/core";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import ProfileScreen from "./ProfileScreen";
+import CommentsScreen from "./CommentsScreen";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import CommentsScreen from "./CommentsScreen";
 import PostList from "./PostList";
+import * as Notifications from "expo-notifications";
+import { isDevice } from "expo-device";
+const appConfig = require("../../../app.json");
 
 const Stack = createNativeStackNavigator();
 
 export class FeedScreen extends Component {
+	async componentDidMount() {
+		var token;
+
+		if (Platform.OS === "android") {
+			await Notifications.setNotificationChannelAsync("default", {
+				name: "default",
+				importance: Notifications.AndroidImportance.MAX,
+				vibrationPattern: [0, 250, 250, 250],
+				lightColor: "#FF231F7C",
+			});
+		}
+		if (isDevice) {
+			const { status: existingStatus } =
+				await Notifications.getPermissionsAsync();
+			if (existingStatus !== "granted") {
+				const { status } =
+					await Notifications.requestPermissionsAsync();
+				if (status !== "granted") {
+					console.log(
+						"Failed to get push token for push notification!"
+					);
+					return;
+				}
+			}
+			const token = (
+				await Notifications.getExpoPushTokenAsync({
+					projectId: appConfig?.expo?.extra?.eas?.projectId,
+				})
+			).data;
+			console.log(token);
+			firebase
+				.firestore()
+				.collection("users")
+				.doc(firebase.auth().currentUser!.uid)
+				.collection("notificationTokens")
+				.doc(token)
+				.set({});
+		} else {
+			console.log("Must use physical device for Push Notifications");
+		}
+	}
+
 	render() {
 		return (
 			<Stack.Navigator
