7f318e399eba327e59527870efebbc17777297dd
alvin-agidi <alvin.agidi@gmail.com>
2024-04-08 00:09:21 +0100

    Added prop for background colour of pressable button

--- src/components/PressableButton.tsx
+++ src/components/PressableButton.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
-import { Pressable, Text, TouchableOpacity } from "react-native";
-import styles from "../globalStyles";
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import globalStyles from "../globalStyles";
 
 export function PressableButton(props: any): JSX.Element {
 	return (
-		<TouchableOpacity {...props} style={styles.button}>
-			<Text style={styles.buttonText}>{props.text}</Text>
+		<TouchableOpacity
+			{...props}
+			style={{
+				...globalStyles.button,
+				backgroundColor: props.backgroundColor ?? "deepskyblue",
+			}}
+		>
+			<Text style={globalStyles.buttonText}>{props.text}</Text>
 		</TouchableOpacity>
 	);
 }
